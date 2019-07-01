30db87b14ac2b69f76b8462878de6d5c8fcb1ca0
Teki Chan <tekichan@gmail.com>
2019-07-01 22:46:37 +0800

    Add libraries in app

--- stockmgr-demo-app/src/App.js
+++ stockmgr-demo-app/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
@@ -13,14 +20,35 @@ const options = {
   }]
 }
 
+// Main App
 function App() {
   return (
-    <div>
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-      />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Form.Control placeholder="Stock Code" />
+        </Col>
+        <Col>
+          <Form.Control as="select">
+            <option>SMA 20</option>
+            <option>SMA 100</option>
+            <option>SMA 250</option>
+            <option>BBand 20</option>
+          </Form.Control>
+        </Col>
+        <Col>
+           <Button variant="primary" type="submit">Refresh</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={options}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
